68ddad02a65a28ac0670c660641001c7b8533bd3
Joshua Hecker <jhecker2001@gmail.com>
2023-10-12 10:02:42 -0500

    Added a case for when the user enters an invalid number.

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -30,7 +30,8 @@ scissors = '''
 rps = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
-print(rps[player_choice])
+if player_choice >= 0 and player_choice <= 2:
+    print(rps[player_choice])
 
 computer_choice = random.randint(0, 2)
 print("Computer chose: ")
@@ -52,10 +53,12 @@ elif player_choice == 1:
         print(message[2])
     else:
         print(message[0])
-else:
+elif player_choice == 2:
     if computer_choice == 0:
         print(message[0])
     elif computer_choice == 1:
         print(message[1])
     else:
         print(message[2])
+else:
+    print("You entered an invalid number, so you lose.")
